9f05ddd29847a4906e77aea4effdc099b0e9d796
Andreas Grech <agnt666@gmail.com>
2023-05-08 14:26:29 +0200

    Adding another chatgpt4 generated robot implementation

--- scripts/RobotsImplementations.js
+++ scripts/RobotsImplementations.js
@@ -527,18 +527,52 @@ const CornerGuardBot = function() {
     let gameContext;
 
     const cornerOffset = 50;
-    const cornerPosition = { x: cornerOffset, y: cornerOffset }; // Top-left corner
+    const corners = [
+        { x: cornerOffset, y: cornerOffset }, // Top-left corner
+        { x: 1024 - cornerOffset, y: cornerOffset }, // Top-right corner
+        { x: cornerOffset, y: 1024 - cornerOffset }, // Bottom-left corner
+        { x: 1024 - cornerOffset, y: 1024 - cornerOffset } // Bottom-right corner
+    ];
+    const cornerPosition = corners[Math.floor(Math.random() * corners.length)];
     const fireDelaySeconds = 0.5;
     let lastFiredTimeSeconds = 0;
     let targetPositionX = 0;
     let targetPositionY = 0;
     let hasTarget = false;
 
+    let lastPositionX = 0;
+    let lastPositionY = 0;
+    let stuckCheckIntervalSeconds = 1;
+    let lastStuckCheckTimeSeconds = 0;
+    let isStuck = false;
+    let stuckRotationTimeSeconds = 0.5;
+    let unstuckStartTimeSeconds = 0;
+
     function moveToCorner(api) {
         const isNearCorner = Math.abs(api.data.positionX - cornerPosition.x) < 10 && Math.abs(api.data.positionY - cornerPosition.y) < 10;
         if (!isNearCorner) {
             api.rotateTowardsPosition(cornerPosition.x, cornerPosition.y);
             api.move();
+        }
+    }
+
+    function checkIfStuck(api, time_seconds) {
+        if (time_seconds - lastStuckCheckTimeSeconds > stuckCheckIntervalSeconds) {
+            const distanceMoved = Math.sqrt(Math.pow(api.data.positionX - lastPositionX, 2) + Math.pow(api.data.positionY - lastPositionY, 2));
+            isStuck = distanceMoved < 5;
+
+            lastPositionX = api.data.positionX;
+            lastPositionY = api.data.positionY;
+            lastStuckCheckTimeSeconds = time_seconds;
+        }
+    }
+
+    function getUnstuck(api, time_seconds) {
+        if (time_seconds - unstuckStartTimeSeconds < stuckRotationTimeSeconds) {
+            api.rotateRight();
+            api.move();
+        } else {
+            isStuck = false;
         }
     }
 
@@ -561,7 +595,13 @@ const CornerGuardBot = function() {
             radar.setFOVAngle_degrees(90);
         },
         update: function(api, time_seconds, delta_seconds) {
-            moveToCorner(api);
+            checkIfStuck(api, time_seconds);
+
+            if (isStuck) {
+                getUnstuck(api, time_seconds);
+            } else {
+                moveToCorner(api);
+            }
 
             const radar = api.radar;
             const scannedAliveRobots = radar.scannedAliveRobots;
@@ -580,6 +620,134 @@ const CornerGuardBot = function() {
                 turret.rotateTowardsPosition(targetPositionX, targetPositionY);
 
                 if (time_seconds - lastFiredTimeSeconds > fireDelaySeconds) {
+                    if (turret.rotateTowardsPosition(targetPositionX, targetPositionY)) {
+                        api.fire(ProjectileTypes.Medium);
+                        lastFiredTimeSeconds = time_seconds;
+                    }
+                }
+            } else {
+                turret.rotateLeft();
+            }
+        }
+    };
+};
+
+const StealthBot = function() {
+    let gameContext;
+
+    const fireDelaySeconds = 0.5;
+    let lastFiredTimeSeconds = 0;
+    let targetPositionX = 0;
+    let targetPositionY = 0;
+    let hasTarget = false;
+
+    const movePatternChangeIntervalSeconds = 0.3;
+    const movePatternDurationSeconds = 1;
+    let currentMovePatternTimeSeconds = 0;
+    let currentMovePattern = 0; // 0: Move forward, 1: Move backward, 2: Rotate left, 3: Rotate right, 4: Zigzag
+
+    let zigzagDirection = true;
+
+    function updateMovePattern(delta_seconds) {
+        if (currentMovePatternTimeSeconds <= 0) {
+            currentMovePattern = Math.floor(Math.random() * 5);
+            currentMovePatternTimeSeconds = movePatternChangeIntervalSeconds;
+        } else {
+            currentMovePatternTimeSeconds -= delta_seconds;
+        }
+    }
+
+    function moveTowardsPosition(api, positionX, positionY) {
+        const dx = positionX - api.data.positionX;
+        const dy = positionY - api.data.positionY;
+        const angleToTarget = Math.atan2(dy, dx) * 180 / Math.PI;
+        api.rotateTowardsAngle_degrees(angleToTarget);
+        api.move();
+    }
+
+    function moveStealthily(api) {
+        if (hasTarget) {
+            moveTowardsPosition(api, targetPositionX, targetPositionY);
+            return;
+        }
+
+        switch (currentMovePattern) {
+            case 0:
+                api.move();
+                break;
+            case 1:
+                api.reverse();
+                break;
+            case 2:
+                api.rotateLeft();
+                break;
+            case 3:
+                api.rotateRight();
+                break;
+            case 4:
+                zigzag(api);
+                break;
+        }
+    }
+
+    function zigzag(api) {
+        if (zigzagDirection) {
+            api.rotateLeft();
+        } else {
+            api.rotateRight();
+        }
+
+        api.move();
+
+        if (Math.random() < 0.1) {
+            zigzagDirection = !zigzagDirection;
+        }
+    }
+
+    function isTargetInRange(api) {
+        const distanceToTarget = Math.sqrt(Math.pow(targetPositionX - api.data.positionX, 2) + Math.pow(targetPositionY - api.data.positionY, 2));
+        return distanceToTarget <= 300;
+    }
+
+    return {
+        name: 'Stealth Bot',
+        create: function(robotSetup) {
+            gameContext = GameContextHolder.gameContext;
+
+            const hullSetup = robotSetup.hull;
+            hullSetup.hullType = RobotHullTypes.Two;
+            hullSetup.hullColor = RobotHullColors.Green;
+
+            const turretSetup = robotSetup.turret;
+            turretSetup.turretType = RobotTurretTypes.Four;
+            turretSetup.turretColor = RobotTurretColors.Blue;
+        },
+        onSpawned: function(api, time_seconds) {
+            const radar = api.radar;
+            radar.radarFollowTurret = true;
+            radar.setFOVAngle_degrees(90);
+        },
+        update: function(api, time_seconds, delta_seconds) {
+            updateMovePattern(delta_seconds);
+            moveStealthily(api);
+
+            const radar = api.radar;
+            const scannedAliveRobots = radar.scannedAliveRobots;
+
+            if (scannedAliveRobots.length > 0) {
+                const closestRobot = scannedAliveRobots[0];
+                targetPositionX = closestRobot.positionX;
+                targetPositionY = closestRobot.positionY;
+                hasTarget = true;
+            } else {
+                hasTarget = false;
+            }
+
+            const turret = api.turret;
+            if (hasTarget) {
+                turret.rotateTowardsPosition(targetPositionX, targetPositionY);
+
+                if (isTargetInRange(api) && time_seconds - lastFiredTimeSeconds > fireDelaySeconds) {
                     if (turret.rotateTowardsPosition(targetPositionX, targetPositionY)) {
                         api.fire(ProjectileTypes.Medium);
                         lastFiredTimeSeconds = time_seconds;
