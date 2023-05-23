ce5256ab6563812e4f5560b3b1814f6f185b826e
Andreas Grech <agnt666@gmail.com>
2023-05-23 05:46:51 +0200

    Removing some obsolete code

--- scripts/ImageDatabase.js
+++ scripts/ImageDatabase.js
@@ -3,15 +3,6 @@
 // TODO: Create a json database which contains all the spritesheets which need to be loaded
 
 const ImageDatabase = (function() {
-    const baseImagesDirectory = 'images';
-    const robotImagesDirectory = `${baseImagesDirectory}/robots`;
-    const projectileImagesDirectory = `${baseImagesDirectory}/Projectiles`;
-    const tracksImagesDirectory = `${baseImagesDirectory}/Tracks`;
-
-    const hullsImagesDirectory = `${robotImagesDirectory}/Hulls`;
-    const turretsImagesDirectory = `${robotImagesDirectory}/Turrets`;
-
-    // const totalTracks = 4;
 
     // TODO: Replace with a json database which contains all the spritesheets which need to be loaded
     const GameElementsSpritesheetKey = "GameElementsSpritesheet",
@@ -24,33 +15,23 @@ const ImageDatabase = (function() {
     };
 
     // Loads all the filenames in the directory
-    const loadImageList = function(filenames, imagesKeys, directory) {
-        console.assert(filenames.length === imagesKeys.length);
-        for (let i = 0; i < filenames.length; i++) {
-            let filename = filenames[i];
-            let fullPath = `${directory}/${filename}`;
-            let imageIdentifier = imagesKeys[i];
+    //const loadImageList = function(filenames, imagesKeys, directory) {
+    //    console.assert(filenames.length === imagesKeys.length);
+    //    for (let i = 0; i < filenames.length; i++) {
+    //        const filename = filenames[i];
+    //        const fullPath = `${directory}/${filename}`;
+    //        const imageIdentifier = imagesKeys[i];
 
-            // Load the image
-            loadImage(imageIdentifier, fullPath);
-        }
-    };
+    //        // Load the image
+    //        loadImage(imageIdentifier, fullPath);
+    //    }
+    //};
 
     const loadAllImages = function() {
 
         // Load arena images
         loadImage('floor_image', 'images/Arena/Floor - Dirt 2 64x64.png');
         loadImage('wall_image', 'images/Arena/Wall - Brick 2 64x64.png');
-
-        //    // Load the tracks images
-        //    for (let i = 0; i < totalTracks; i++) {
-        //        let track = `Track_${i + 1}`;
-        //        let trackAIdentifier = `${track}_A`;
-        //        let trackBIdentifier = `${track}_B`;
-
-        //        loadImage(trackAIdentifier, `${tracksImagesDirectory}/${trackAIdentifier}.png`);
-        //        loadImage(trackBIdentifier, `${tracksImagesDirectory}/${trackBIdentifier}.png`);
-        //    }
 
         //Load the game elements spritesheet
         const gameContext = GameContextHolder.gameContext;
