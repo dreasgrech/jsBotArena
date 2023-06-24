08da1ccb83db9eb5dce6db45787d6e038d4e833e
Andreas Grech <agnt666@gmail.com>
2023-06-24 16:30:17 +0200

    TODO

--- scripts/GameContextHolder.js
+++ scripts/GameContextHolder.js
@@ -10,6 +10,7 @@ const GameContextHolder = (function() {
          * @type {Phaser.Scene}
          */
         scene: null,
+        // TODO: Might be best to extract the gameTime and deltaTime from here because they're accessed per-frame
         gameTime: 0,
         deltaTime: 0
     };
