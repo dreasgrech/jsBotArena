bb0f120f4bc2e00f94bb46585114e9e724b825bb
Andreas Grech <agnt666@gmail.com>
2023-08-23 23:24:20 +0200

    Fixing an issue with the UI panel where sometimes an exception was being thrown when unloading the level

--- scripts/UIRobotInfoPanel.js
+++ scripts/UIRobotInfoPanel.js
@@ -1,57 +1,55 @@
 "use strict";
 
 const UIRobotInfoPanel = (function() {
-    const panelWidth = 230; // Width of the panel
-    const panelHeight = 900; // Height of the panel
+    const PANEL_WIDTH = 230; // Width of the panel
+    const PANEL_HEIGHT = 900; // Height of the panel
 
-    const panelY = 0; // Y position of the panel
+    const PANEL_Y = 0; // Y position of the panel
+    
+    const PANEL_DEPTH = GameObjectDepths.UI_RobotInformationPanel;
+    const PANEL_TEXT_DEPTH = PANEL_DEPTH + 1;
 
     const robotsIndex = [];
     const robotInfoTexts = [];
-
-    const panelDepth = GameObjectDepths.UI_RobotInformationPanel;
-    const panelTextDepth = panelDepth + 1;
-
-    let titleText;
 
     const obj = {
         create: function() {
             const gameContext = GameContextHolder.scene;
 
-            const panelX = gameContext.cameras.main.width - panelWidth; // X position of the panel
+            const panelX = gameContext.cameras.main.width - PANEL_WIDTH; // X position of the panel
 
             // Create a Graphics object for the panel background
             const panelGraphics = gameContext.add.graphics();
-            panelGraphics.depth = panelDepth;
+            panelGraphics.depth = PANEL_DEPTH;
             panelGraphics.fillStyle(0x333333, 0.3); // Set panel background color and alpha
-            panelGraphics.fillRect(panelX, panelY, panelWidth, panelHeight); // Draw the panel background
+            panelGraphics.fillRect(panelX, PANEL_Y, PANEL_WIDTH, PANEL_HEIGHT); // Draw the panel background
 
             // Create a Text object for the panel title
-            titleText = gameContext.add.text(panelX + panelWidth / 2,
-                panelY + 10,
+            const titleText = gameContext.add.text(panelX + PANEL_WIDTH / 2,
+                PANEL_Y + 10,
                 'Robot Information',
                 {
                     fontSize: '19px',
                     color: '#ffffff'
                 }).setOrigin(0.5, 0);
-            titleText.depth = panelTextDepth;
+            titleText.depth = PANEL_TEXT_DEPTH;
         },
         add: function(robotIndex) {
             const gameContext = GameContextHolder.scene;
 
-            robotsIndex.push(robotIndex);
-
-            const panelX = gameContext.cameras.main.width - panelWidth; // X position of the panel
+            const panelX = gameContext.cameras.main.width - PANEL_WIDTH; // X position of the panel
 
             const infoText = gameContext.add.text(panelX + 10,
-                panelY + 30 + robotIndex * 90,
+                PANEL_Y + 30 + robotIndex * 90,
                 '',
                 {
                     fontSize: '10px',
                     color: '#ffffff'
                 });
-            infoText.depth = panelTextDepth;
+            infoText.depth = PANEL_TEXT_DEPTH;
             //robotInfoTexts.push(infoText);
+            
+            robotsIndex.push(robotIndex);
             robotInfoTexts[robotIndex] = infoText;
         },
         update: function() {
@@ -89,7 +87,7 @@ Turret Rotation: ${turretRotation.toFixed(2)}�
 //Turret Rotation: ${Phaser.Math.RadToDeg(turretRotation).toFixed(2)}�
                 robotInfoTexts[robotIndex].setText(infoText);
                 if (robotHealth === 0) {
-                    robotInfoTexts[robotIndex].setColor('#ff0000');
+                    robotInfoTexts[robotIndex].setColor('#282c34');
                 }
                 //Logger.log(robotInfoTexts[robotIndex]);
 
@@ -97,11 +95,14 @@ Turret Rotation: ${turretRotation.toFixed(2)}�
         },
         //system_unloadLevel: function(){
         clearPanel: function(){
-            const robotInfoTextsLength = robotInfoTexts.length;
-            for (let i = 0; i < robotInfoTextsLength; i++) {
-                const robotInfoText = robotInfoTexts[i];
+            for (let i = 0; i < robotsIndex.length; i++) {
+                let robotIndex = robotsIndex[i];
+                const robotInfoText = robotInfoTexts[robotIndex];
                 robotInfoText.destroy();
             }
+            
+            robotsIndex.length = 0;
+            robotInfoTexts.length = 0;
         }
     };
 
